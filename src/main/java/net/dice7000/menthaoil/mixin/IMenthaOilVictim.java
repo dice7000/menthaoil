36fb33b524c924b32f93120ef50e15505686c20d
dice7000 <noreply@abcdefggfedcba.aiueo>
2026-08-25 23:01:35 +0900

    hotfix 2

--- src/main/java/net/dice7000/menthaoil/mixin/IMenthaOilVictim.java
+++ src/main/java/net/dice7000/menthaoil/mixin/IMenthaOilVictim.java
@@ -1,6 +1,23 @@
 package net.dice7000.menthaoil.mixin;
 
+import net.dice7000.menthaoil.MORegistry;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
+
 public interface IMenthaOilVictim {
     void menthaoil$setAffected();
     boolean menthaoil$getAffected();
+
+    static void tick(Entity entity, int tickCount) {
+        if (entity.onGround()) entity.setDeltaMovement(new Vec3((Math.random() * 2 - 1), Math.random() * 0.5 + 0.5, (Math.random() * 2 - 1)));
+        if (tickCount % 10 == 0) {
+            if (entity instanceof LivingEntity l) l.hurt(MORegistry.causeDeathMintDamage(l.level()), l.getMaxHealth() * 0.1F);
+            if (!(entity instanceof Player)) {
+                entity.setXRot((float) (Math.random() * 180 - 90));
+                entity.setYRot((float) (Math.random() * 360 - 180));
+            }
+        }
+    }
 }
--- src/main/java/net/dice7000/menthaoil/mixin/mixin/EntityMixin.java
+++ src/main/java/net/dice7000/menthaoil/mixin/mixin/EntityMixin.java
@@ -1,14 +1,9 @@
 package net.dice7000.menthaoil.mixin.mixin;
 
-import net.dice7000.menthaoil.MORegistry;
 import net.dice7000.menthaoil.mixin.IMenthaOilVictim;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,26 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin implements IMenthaOilVictim {
-    @Unique private final Entity menthaoil$this = (Entity) (Object) this;
-    @Shadow private boolean onGround;
-    @Shadow private Vec3 deltaMovement;
-    @Shadow private Level level;
-    @Shadow private float xRot;
-    @Shadow private float yRot;
     @Unique private boolean menthaoil$hasAffected;
     @Unique private int menthaoil$count;
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void tickInject(CallbackInfo ci) {
-        if (menthaoil$getAffected()) {
-            if (onGround) deltaMovement = new Vec3((Math.random() * 2 - 1), Math.random() * 0.5 + 0.5, (Math.random() * 2 - 1));
-            if (menthaoil$count % 10 == 0) {
-                xRot = (float) (Math.random() * 360 - 180);
-                yRot = (float) (Math.random() * 180 - 90);
-                if (menthaoil$this instanceof LivingEntity l) l.hurt(MORegistry.causeDeathMintDamage(level), l.getMaxHealth() * 0.1F);
-            }
-            menthaoil$count++;
-        }
+        if (menthaoil$getAffected()) IMenthaOilVictim.tick((Entity) (Object) this, menthaoil$count++);
     }
     @Inject(method = "saveWithoutId", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/world/entity/Entity;addAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V",
